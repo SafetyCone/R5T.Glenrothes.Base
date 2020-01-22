4dc38fded72166e1685e938ec3f714a1fedf0540
MinexAutomation <minex.automation@gmail.com>
2020-01-22 00:27:52 -0800

    Added the ITestingDataDirectoryPathProvider service definition interface type.

--- source/R5T.Glenrothes.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Glenrothes.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Glenrothes.Base/Code/Services/Definitions/ITestingDataDirectoryPathProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Glenrothes
+{
+    public interface ITestingDataDirectoryPathProvider
+    {
+        string GetTestingDataDirectoryPath();
+    }
+}
